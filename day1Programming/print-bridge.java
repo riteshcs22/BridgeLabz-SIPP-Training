8a19b94d6c13da900d5e3e880a089a1df448aedb
Ritesh chauhan <142732768+riteshcs22@users.noreply.github.com>
2025-07-31 23:19:45 +0530

    print-bridgelabz

--- day1Programming/print-bridge.java
+++ day1Programming/print-bridge.java
@@ -3,6 +3,6 @@ import java.util.Scanner;
 class PrintBridge {
   public static void main(String[] args) {
 
-    System.out.print("Enter the number of rows for the bridge: ");
+    System.out.print("Welcome to Bridgelabz!");
   }
 }
