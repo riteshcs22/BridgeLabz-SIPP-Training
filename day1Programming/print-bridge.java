249612fc95fe88ee1fda3e63c1a68819e161e63f
Ritesh chauhan <142732768+riteshcs22@users.noreply.github.com>
2025-07-31 22:59:29 +0530

    first commit

---
+++ day1Programming/print-bridge.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+class PrintBridge {
+  public static void main(String[] args) {
+
+    System.out.print("Enter the number of rows for the bridge: ");
+  }
+}
---
+++ file.tsx
@@ -0,0 +1,6 @@
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/riteshcs22/BridgeLabz-SIPP-Training.git
+git push -u origin main
