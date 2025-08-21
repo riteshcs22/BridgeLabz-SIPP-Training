6dc69902b8ba3e9c21af9e424a9609e1724efd90
riteshcs22 <ritesh.chauhan_cs22@gla.ac.in>
2025-08-21 17:14:14 +0530

    day4 completed

---
+++ Day4_JavaConstructors/Level-1/HotelBooking.java
@@ -0,0 +1,42 @@
+public class HotelBooking {
+    String guestName;
+    String roomType;
+    int nights;
+
+    public HotelBooking() {
+        this("Unknown", "Standard", 1);
+    }
+
+    public HotelBooking(String guestName, String roomType, int nights) {
+        this.guestName = guestName;
+        this.roomType = roomType;
+        this.nights = nights;
+    }
+
+    public HotelBooking(HotelBooking other) {
+        this.guestName = other.guestName;
+        this.roomType = other.roomType;
+        this.nights = other.nights;
+    }
+
+    public void displayBooking() {
+        System.out.println("Guest Name: " + guestName);
+        System.out.println("Room Type: " + roomType);
+        System.out.println("Nights: " + nights);
+    }
+
+    public static void main(String[] args) {
+        HotelBooking defaultBooking = new HotelBooking();
+        HotelBooking customBooking = new HotelBooking("John Doe", "Deluxe", 3);
+        HotelBooking copiedBooking = new HotelBooking(customBooking);
+
+        System.out.println("Default Booking:");
+        defaultBooking.displayBooking();
+
+        System.out.println("\nCustom Booking:");
+        customBooking.displayBooking();
+
+        System.out.println("\nCopied Booking:");
+        copiedBooking.displayBooking();
+    }
+}
